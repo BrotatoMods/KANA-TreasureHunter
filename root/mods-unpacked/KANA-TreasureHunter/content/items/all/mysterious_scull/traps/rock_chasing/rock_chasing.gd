55a2303d385db1054cef6557e41186ad8934d501
Kai <kai@kana.jetzt>
2023-11-13 09:49:36 +0100

    refactor: :recycle: improved rock rolling animation

---
+++ root/mods-unpacked/KANA-TreasureHunter/content/items/all/mysterious_scull/traps/rock_chasing/rock_chasing.gd
@@ -0,0 +1,9 @@
+extends Enemy
+
+
+func update_animation(movement:Vector2) -> void:
+	if mirror_sprite_with_movement:
+		if movement.x > 0:
+			_animation_player.play("roll_forwards")
+		elif movement.x < 0:
+			_animation_player.play("roll_backwards")
