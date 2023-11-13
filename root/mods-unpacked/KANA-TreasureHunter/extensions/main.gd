2b25b28c2a8398f361a956fb992b59237c312b82
Kai <kai@kana.jetzt>
2023-11-13 11:25:38 +0100

    feat: :sparkles: replace crate with scull

--- root/mods-unpacked/KANA-TreasureHunter/extensions/main.gd
+++ root/mods-unpacked/KANA-TreasureHunter/extensions/main.gd
@@ -4,6 +4,10 @@ extends "res://main.gd"
 func on_consumable_picked_up(consumable: Node) -> void:
 	.on_consumable_picked_up(consumable)
 
+	if consumable.consumable_data.my_id == "kana_consumable_mysterious_scull" and RunData.effects["item_box_gold"] != 0:
+		RunData.add_gold(RunData.effects["item_box_gold"])
+		RunData.tracked_item_effects["item_bag"] += RunData.effects["item_box_gold"]
+
 	# If a crate got collected
 	if consumable.consumable_data.my_id == "consumable_item_box":
 		RunData.tracked_item_effects["character_treasure_hunter"] += 1
