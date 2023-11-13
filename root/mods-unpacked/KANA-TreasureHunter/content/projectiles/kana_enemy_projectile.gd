e1f79983b175f2ecb1d65676c1d8e90e6ea98985
Kai <kai@kana.jetzt>
2023-11-13 09:07:15 +0100

    feat: :sparkles: added damage scaling to projectiles

--- root/mods-unpacked/KANA-TreasureHunter/content/projectiles/kana_enemy_projectile.gd
+++ root/mods-unpacked/KANA-TreasureHunter/content/projectiles/kana_enemy_projectile.gd
@@ -1,11 +1,17 @@
 class_name KANAEnemyProjectile
-extends EnemyProjectile
+extends "res://projectiles/bullet_enemy/enemy_projectile.gd"
 
 
 export(Array, Resource) var hit_sounds
 export(Array, float) var volume_mods
+export(int) var damage_base
+export(float) var damage_increase_each_wave
 
 
-func play_sound() -> void:
+func _ready() -> void:
+	.call_deferred("set_damage", (damage_base + (RunData.current_wave - 1) * damage_increase_each_wave) as int)
+
+
+func kana_play_sound() -> void:
 	var hit_sound_index = Utils.get_random_int(0, hit_sounds.size() - 1)
 	SoundManager.play(hit_sounds[hit_sound_index], volume_mods[hit_sound_index], 0.2)
